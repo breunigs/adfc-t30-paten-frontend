94f2093a3f169af277e9623b1a5e3d684a41d339
Sven Anders <sven@anders-hamburg.de>
2019-01-06 14:30:15 +0100

    use prod to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ver=version3
-ng build --base-href=https://tools.adfc-hamburg.de/t30-paten/${ver}/ --aot --deploy-url=https://tools.adfc-hamburg.de/t30-paten/${ver}/ &&
+ng build --base-href=https://tools.adfc-hamburg.de/t30-paten/${ver}/ --prod --deploy-url=https://tools.adfc-hamburg.de/t30-paten/${ver}/ &&
     sed -i -e 's/\(data-base-url="\)."/\1\/t30-paten\/'${ver}'"/' dist/t30-paten/index.html &&
     rsync -r --delete -v dist/t30-paten/ root@tools:/var/www/html/t30-paten/${ver}
