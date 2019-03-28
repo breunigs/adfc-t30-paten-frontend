68b2c11bf8716ee4c96b72a9fd51bf1ffb030cd1
Sven Anders <sven@anders-hamburg.de>
2019-03-28 13:06:39 +0100

    change production backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   VERSION: require('../../package.json').version,
-  API_BASE_URL: 'https://merlin.anders.hamburg/t30-paten-backen/production/'
+  API_BASE_URL: 'https://tools.adfc-hamburg.de/t30-paten/backend/'
 };
